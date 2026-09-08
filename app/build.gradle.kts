2268af5bb50c7ac912bd1961fe80ea686ca53d79
Yumesion <Yumesion@users.noreply.github.com>
2026-09-08 15:43:17 +0000

    Rebrand to Scan Yume (app name + applicationId)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
     namespace = "eu.kanade.tachiyomi"
 
     defaultConfig {
-        applicationId = "app.mihon"
+        applicationId = "app.scanyume"
 
         versionCode = 29
         versionName = "0.20.4"
