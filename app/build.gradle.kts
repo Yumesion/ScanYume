7f2228e68492430ec2b6384ede25455d7cc62baa
Yumesion <Yumesion@users.noreply.github.com>
2026-09-09 17:00:23 +0000

    New Japanese autumn launcher icon (red maple leaf) and bump to v0.20.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
     defaultConfig {
         applicationId = "app.scanyume"
 
-        versionCode = 30
-        versionName = "0.20.5"
+        versionCode = 31
+        versionName = "0.20.6"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
